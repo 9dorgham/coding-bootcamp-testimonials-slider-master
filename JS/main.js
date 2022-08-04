bb39aa493be329c3f0a17364ae4aa3e03c40ea33
9dorgham <am8505841@gmail.com>
2022-08-04 21:34:58 +0200

    0.1v

---
+++ JS/main.js
@@ -0,0 +1,75 @@
+// selectors
+let hero = document.querySelector('.hero');
+let comment = document.querySelector('#commnet');
+let name = document.querySelector('.name');
+let job = document.querySelector('.job');
+let prev = document.querySelector('.prev');
+let next = document.querySelector('.next');
+
+async function get() {
+  let url = "/JS/data.json";
+  let req = await fetch(url);
+  let arr = await req.json();
+
+  start(arr);
+}
+get();
+
+
+
+
+function start(arr) {
+  create(arr[0])
+
+  let count = 0;
+
+  prev.addEventListener('click', () => {
+    count--;
+    if (count < 0) {
+      count = arr.length - 1;
+    }
+    create(arr[count]);
+    show();
+  });
+  next.addEventListener('click', () => {
+    count++;
+    if(count > arr.length - 1) {
+      count = 0;
+    }
+    create(arr[count]);
+    show();
+  });
+};
+
+// DOM
+function create(ob) {
+  for (let i in ob) {
+    if (i == "src") {
+      hero.src = ob[i];
+    }else if (i == "comment") {
+      comment.innerHTML = ob[i];
+    }else if (i == "fullname") {
+      typeWriting(ob[i], name);
+    }else {
+      job.textContent = ob[i];
+    }
+  }
+}
+
+
+function typeWriting(str, ele) {
+  ele.innerHTML = '';
+  let i = 0;
+  let id = setInterval(() => {
+    ele.textContent += str[i];
+    i++;
+    if (i == str.length) clearInterval(id);
+  }, 100);
+}
+
+function show() {
+  hero.classList.add('ani');
+  setTimeout(() => {
+    hero.classList.remove('ani');
+  }, 200);
+}
